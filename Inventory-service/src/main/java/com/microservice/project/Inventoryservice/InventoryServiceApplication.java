202653558043a1ec8bd0e69966ea7f5a7a6beca5
Tushar More <65149782+Tushar443@users.noreply.github.com>
2024-04-10 20:32:14 +0530

    Product Service, Order Service Inventory Service committed.

---
+++ Inventory-service/src/main/java/com/microservice/project/Inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,33 @@
+package com.microservice.project.Inventoryservice;
+
+import com.microservice.project.Inventoryservice.Repository.InventoryRepo;
+import com.microservice.project.Inventoryservice.model.Inventory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepo inventoryRepo){
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("Iphone_13");
+			inventory.setQuantity(100);
+
+			Inventory inventory2 = new Inventory();
+			inventory2.setSkuCode("Iphone_13_red");
+			inventory2.setQuantity(0);
+
+			inventoryRepo.save(inventory);
+			inventoryRepo.save(inventory2);
+		};
+	}
+
+}
